31741cda2097f6e0dc60fc4a83e09dc3a483bd67
Igal Tabachnik <hmemcpy@gmail.com>
2014-10-11 16:43:08 +0300

    Adding work to parse Java snippets (e.g. single methods, not entire classes)

---
+++ src/ReSharper.ExJava.Tests/ArrayParsingTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace ReSharper.ExJava.Tests
+{
+    public class ArrayParsingTests
+    {
+        [Fact]
+        public void ArrayWithInitializerParsedCorrectly()
+        {
+            string javaArray = @"
+public void foo() {
+  byte bytes[] = new byte[] { (byte)'a', (byte)'b', (byte)'c', (byte)'d' };
+}";
+            string result = JavaToCSharp.JavaToCSharpConverter.ConvertText(javaArray);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ src/ReSharper.ExJava.Tests/JavaSnippetParserTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JavaToCSharp;
+using Shouldly;
+using Xunit;
+
+namespace ReSharper.ExJava.Tests
+{
+    public class JavaSnippetParserTests
+    {
+        [Fact]
+        public void CanParseMethods()
+        {
+            string java = "public void foo() {}";
+            string csharp = 
+@"public void Foo()
+{
+}";
+            string result = JavaSnippetParser.Parse(java);
+            
+            result.ShouldBe(csharp);
+        }
+
+        [Fact]
+        public void JavaMethodsAreNotVirtualByDefault()
+        {
+            
+        }
+    }
+}
